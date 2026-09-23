87daea38f7c93e9b7c48b7f4a8d001d1bbbfc4df
Sterlid <ofdeathequinox@gmail.com>
2026-09-23 17:43:46 +0800

    CSDT4-10: Group market API operations by feature in Swagger

--- src/main/java/sg/edu/smu/cs203/market/price/MarketPriceController.java
+++ src/main/java/sg/edu/smu/cs203/market/price/MarketPriceController.java
@@ -4,12 +4,14 @@ import java.time.Instant;
 import java.util.List;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "F1: Market prices")
 @RestController
 @RequestMapping("/api/v1/market-prices")
 public class MarketPriceController {
